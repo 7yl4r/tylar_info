bf9bdffa8148075eb75bb0a4d54cb269bc5cb80d
Tylar <murray.tylar@gmail.com>
2023-12-27 12:03:28 -0500

    + custom fn & config post

---
+++ fn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+newpost () {
+  echo "creating post named $1..."
+  POSTNAME=$1
+  POSTPATH=posts/$POSTNAME/index.qmd
+  mkdir posts/$POSTNAME
+  cp post_template.qmd $POSTPATH
+  #emacs $POSTPATH
+}
